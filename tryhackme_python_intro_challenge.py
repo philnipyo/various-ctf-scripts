810e53322fad1f84331603218ac98a08c5bccf46
philnipyo <philnipyo@yahoo.com>
2020-09-15 13:11:57 -0700

    Condense 3 for loops into 1

--- tryhackme_python_intro_challenge.py
+++ tryhackme_python_intro_challenge.py
@@ -1,8 +1,8 @@
-# Notes
-# Encoded 15 times!
+# Notes: Encoded 15 times:
 # 5 times encoded using base 64
 # 5 times encoded using base 32
 # 5 times encoded using base 16
+# Reverse the decoding order (16->32->64 instead of 64->32->16)
 
 # Import the necessary base64 library
 from base64 import *
@@ -14,16 +14,16 @@ rawFlag = open("encodedflag.txt", "r")
 # The retrieved line is a string.
 target = rawFlag.readline()
 
-# Loop 5 times decoding the line using base16
-for i in range(1, 6):
-    target = b16decode(target)
 
-# Loop 5 times dcoding the line using base32
-for i in range(1, 6):
-    target = b32decode(target)
-
-# Loop 5 times dcoding the line using base64
-for i in range(1, 6):
-    target = b64decode(target)
-
-print(target)
+for i in range(1, 16):
+    # Loop 5 times decoding the line using base16
+    if i >= 1 and i < 6:
+        target = b16decode(target)
+    # Loop 5 times dcoding the line using base32
+    elif i >=6 and i < 11:
+        target = b32decode(target)
+    # Loop 5 times dcoding the line using base64
+    elif i >= 11 and i < 16:
+        target = b64decode(target)
+else:
+    print(target)
